d863a232afe73f7fbe06492ca8033e90d873348f
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-23 05:59:07 +0200

    renaming fetch helper

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,10 @@
-import useFetch from "../hooks/useFetch";
 import Link from "next/link";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import fetchData from "../helpers/fetchData";
 
 export const getStaticPaths = async () => {
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}/blogs`;
-  const data = await useFetch(url);
+  const data = await fetchData(url);
 
   const paths = data.map((path) => {
     return {
@@ -22,7 +22,7 @@ export const getStaticProps = async (context) => {
   const id = context.params.id;
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}/blogs/${id}`;
 
-  const data = await useFetch(url);
+  const data = await fetchData(url);
 
   return {
     props: { post: data },
@@ -46,7 +46,6 @@ const BlogDetails = ({ post }) => {
       {!post && (
         <>
           <h2>Post Not Found</h2>
-          <p>Well, that's disappointing.</p>
           <p>
             <Link href="/">Visit Our Homepage</Link>
           </p>
