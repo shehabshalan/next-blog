302fcd65c1cc4e9f83d20612aa32efa67738d7db
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-23 05:47:07 +0200

    blog card component

---
+++ components/BlogCard.js
@@ -0,0 +1,52 @@
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
+
+const BlogCard = ({ blog, blogId }) => {
+  return (
+    <Card
+      sx={{
+        mb: { xs: 0, md: 5 },
+        mt: { xs: 2 },
+        maxWidth: "900px",
+        cursor: "pointer",
+      }}
+    >
+      <CardHeader
+        title={blog.title}
+        // subheader={toDateTime(blog.datetime.seconds)}
+      />
+      <Link href={`/${blogId}`}>
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {blog.body.length <= 25
+              ? blog.body
+              : `${blog.body.slice(0, 300)} ...`}
+          </Typography>
+        </CardContent>
+      </Link>
+
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite sx={{ color: "red" }} />}
+          />
+        </IconButton>
+        {/* <IconButton aria-label="share">
+          <Share />
+        </IconButton> */}
+      </CardActions>
+    </Card>
+  );
+};
+
+export default BlogCard;
