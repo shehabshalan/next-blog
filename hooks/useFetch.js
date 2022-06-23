a3b9d24cc6f07ad9f40ae956749f0a74a6eb7915
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-23 05:47:36 +0200

    fetch helper hook

---
+++ hooks/useFetch.js
@@ -0,0 +1,17 @@
+const useFetch = async (url) => {
+  const header = {
+    Authorization: process.env.NEXT_PUBLIC_BEARER_TOKEN,
+  };
+
+  try {
+    const res = await fetch(url, {
+      headers: header,
+    });
+    const { data } = await res.json();
+    return data;
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export default useFetch;
