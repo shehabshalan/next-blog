bb9102827c8e2ee9136754f6ffd393c1a0186739
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-26 02:56:20 +0200

    title change

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const Blogs = ({ blogs }) => {
   return (
     <Box flex={8} p={{ xs: 0, md: 2 }}>
       <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
-        All Blogs
+        Blogs
       </Typography>
       {blogs.map((blog) => (
         <BlogCard key={blog.id} blogId={blog.id} blog={blog.attributes} />
