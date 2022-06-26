3f4a9bdc7234713120be2f23144f04094b9c8697
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-26 02:55:45 +0200

    app theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function MyApp({ Component, pageProps }) {
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#000000",
+      },
+      secondary: {
+        main: "#000000",
+      },
+    },
+  });
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
