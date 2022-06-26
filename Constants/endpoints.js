907f1c1117013e247e1ea38768d88de2c59fbb89
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-27 00:13:33 +0200

    login and register endpoints added

--- Constants/endpoints.js
+++ Constants/endpoints.js
@@ -5,6 +5,8 @@ export class Endpoints {
       : "https://reactblog-strapi.herokuapp.com/api/";
   static getBlogs = Endpoints.baseUrl + "blogs";
   static getBlogById = Endpoints.baseUrl + "blogs";
+  static login = Endpoints.baseUrl + "auth/local";
+  static register = Endpoints.baseUrl + "auth/local/register";
   static getToken =
     process.env.NODE_ENV === "development"
       ? process.env.NEXT_PUBLIC_BEARER_TOKEN_DEV
