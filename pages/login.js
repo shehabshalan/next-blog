6fc87c96f7fd3cc90c2d0350f27f516d70e89458
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-27 03:24:05 +0200

    redirect after login

--- pages/login.js
+++ pages/login.js
@@ -8,9 +8,13 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import LoadingButton from "@mui/lab/LoadingButton";
 import { Endpoints } from "../Constants/endpoints";
+import axios from "axios";
+import { useRouter } from "next/router";
+
 const login = () => {
   const [loading, setLoading] = React.useState(false);
-  const handleSubmit = (event) => {
+  const router = useRouter();
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
@@ -18,22 +22,22 @@ const login = () => {
       identifier: data.get("email"),
       password: data.get("password"),
     };
-    console.log(payload);
-    const url = Endpoints.login;
-    // post request to server
+
     setLoading(true);
-    fetch(url, {
-      method: "POST",
-      body: payload,
-    })
-      .then((res) => res.json())
-      .then((data) => {
+    const url = Endpoints.login;
+    axios
+      .post(url, payload)
+      .then((response) => {
         setLoading(false);
-        console.log(data);
+        console.log("User profile", response.data.user);
+        console.log("User token", response.data.jwt);
+        localStorage.setItem("token", response.data.jwt);
+        localStorage.setItem("userId", response.data.user.userId);
+        window.location.href = "/";
       })
-      .catch((err) => {
+      .catch((error) => {
         setLoading(false);
-        console.log(err);
+        console.log("An error occurred:", error.response);
       });
   };
 
@@ -49,43 +53,50 @@ const login = () => {
           p: 5,
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <LoadingButton
-            type="submit"
-            fullWidth
-            loading={loading}
-            variant="contained"
-            color="secondary"
-            sx={{ mt: 3, mb: 2 }}
+        <Box sx={{ maxWidth: "500px" }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
           >
-            Login{" "}
-          </LoadingButton>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <LoadingButton
+              type="submit"
+              fullWidth
+              loading={loading}
+              variant="contained"
+              color="secondary"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login{" "}
+            </LoadingButton>
+          </Box>
         </Box>
       </Box>
     </ContentPaper>
