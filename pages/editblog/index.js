594a6a6719dea374c022d976ea9be1bcbac14577
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-28 03:26:19 +0200

    blog edit page

---
+++ pages/editblog/index.js
@@ -0,0 +1,117 @@
+import React, { useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useRouter } from "next/router";
+
+import axios from "axios";
+import { Endpoints } from "../../Constants/endpoints";
+import ContentPaper from "../../components/ContentPaper";
+import { useUserAuth } from "../../context/UserAuthContext";
+
+const EditBlog = ({ blog }) => {
+  const { user } = useUserAuth();
+
+  /// check if the logged in user's id is the same as the blog's author id
+  const isAuthor = () => {
+    return user?.userId === "ef87715f-6bb9-4f01-8620-f5b409c5a345";
+  };
+
+  useEffect(() => {
+    console.log(isAuthor());
+    if (!isAuthor()) {
+      router.push("/");
+    }
+  }, [isAuthor]);
+
+  const router = useRouter();
+  const [loading, setLoading] = React.useState(false);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const payload = {
+      data: {
+        title: data.get("title"),
+        body: data.get("body"),
+        userId: user.userId,
+      },
+    };
+    console.log(payload);
+    setLoading(true);
+    const url = Endpoints.getBlogs;
+    axios
+      .put(url, payload, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        setLoading(false);
+        console.log("blog published", response.data);
+        router.push("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log("An error occurred:", error.response);
+      });
+  };
+  return (
+    <ContentPaper>
+      <Box
+        sx={{
+          mt: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          p: 5,
+        }}
+      >
+        <Box sx={{ maxWidth: "500px" }}>
+          <Typography component="h1" variant="h5">
+            Edit blog
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="title"
+              label="Title"
+              name="title"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="body"
+              label="Body"
+              type="textarea"
+              id="body"
+              multiline
+              rows={4}
+            />
+            <LoadingButton
+              type="submit"
+              fullWidth
+              loading={loading}
+              variant="contained"
+              color="secondary"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Publish{" "}
+            </LoadingButton>
+          </Box>
+        </Box>
+      </Box>
+    </ContentPaper>
+  );
+};
+
+export default EditBlog;
