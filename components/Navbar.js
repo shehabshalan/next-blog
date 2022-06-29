26f78d2a9a0f365311d106bd75497f93ceeb54da
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-29 06:24:51 +0200

    removed search to its own comp

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,55 +6,13 @@ import {
   Grid,
   Button,
 } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { styled, alpha } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
-import { useEffect, useState } from "react";
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 /// to do - convert auth buttons to small burger menu and remove all buttons on small screen
 /// to do - implement search
 
 import Link from "next/link";
 import { useUserAuth } from "../context/UserAuthContext";
+import SearchBar from "./Search";
 const Navbar = () => {
   const { isAuth, logout } = useUserAuth();
   return (
@@ -75,18 +33,8 @@ const Navbar = () => {
                 </Typography>
               </Link>
             </Grid>
-            <Grid item xs={6} md={6} lg={6}>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search…"
-                  inputProps={{ "aria-label": "search" }}
-                />
-              </Search>
-            </Grid>
 
+            <SearchBar />
             {isAuth ? (
               <Grid item xs={4} md={4} lg={4}>
                 <Link href="/createblog">
