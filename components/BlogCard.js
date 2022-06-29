24e55e8fcc4f658173a93ca41372630628e49a47
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-29 02:21:34 +0200

    menu and delete blog button

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Favorite,
   FavoriteBorder,
@@ -6,6 +7,7 @@ import {
   ChatBubbleOutline,
 } from "@mui/icons-material";
 import {
+  Avatar,
   Box,
   Card,
   CardActions,
@@ -16,11 +18,37 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { red } from "@mui/material/colors";
 import Link from "next/link";
 import { useUserAuth } from "../context/UserAuthContext";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import axios from "axios";
+import mutateData from "../helpers/mutateData";
+import { Endpoints } from "../Constants/endpoints";
+const options = ["Delete", "Edit"];
+
+const ITEM_HEIGHT = 48;
 
 const BlogCard = ({ blog, blogId }) => {
   const { user } = useUserAuth();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (e) => {
+    // console log the value of the selected option
+    const selectedOption = e.target.innerText;
+    if (selectedOption === "Delete") {
+      mutateData(`${Endpoints.deleteBlog}/${blogId}`);
+    }
+    if (selectedOption === "Edit") {
+      console.log("edit");
+    }
+    setAnchorEl(null);
+  };
 
   return (
     <Card
@@ -32,6 +60,55 @@ const BlogCard = ({ blog, blogId }) => {
       }}
     >
       <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {blog.username.charAt(0)}
+          </Avatar>
+        }
+        action={
+          <>
+            {user?.userId === blog.userId ? (
+              <div>
+                <IconButton
+                  aria-label="more"
+                  id="long-button"
+                  aria-controls={open ? "long-menu" : undefined}
+                  aria-expanded={open ? "true" : undefined}
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+                <Menu
+                  id="long-menu"
+                  MenuListProps={{
+                    "aria-labelledby": "long-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  PaperProps={{
+                    style: {
+                      maxHeight: ITEM_HEIGHT * 4.5,
+                      width: "20ch",
+                      left: "850px",
+                    },
+                  }}
+                >
+                  {options.map((option) => (
+                    <MenuItem key={option} onClick={handleClose}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </div>
+            ) : null}
+          </>
+        }
+        title={blog.username}
+        // subheader="September 14, 2016"
+      />
+      <CardHeader
         title={blog.title}
         // subheader={toDateTime(blog.datetime.seconds)}
       />
@@ -42,15 +119,6 @@ const BlogCard = ({ blog, blogId }) => {
               ? blog.body
               : `${blog.body.slice(0, 300)} ...`}
           </Typography>
-          {/* {user?.userId === blog.userId ? (
-            <IconButton
-              aria-label="edit"
-              sx={{ float: "right" }}
-              href={`/editblog/${blogId}`}
-            >
-              Edit this
-            </IconButton>
-          ) : null} */}
         </CardContent>
       </Link>
 
