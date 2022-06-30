c273c0245e7ce2960c8ef5c4cd48fc3978bf0ff8
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-30 02:40:48 +0200

    removed prop

--- components/CommentFeed.js
+++ components/CommentFeed.js
@@ -9,13 +9,13 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const CommentFeed = ({ commentId, comment }) => {
+const CommentFeed = ({ comment }) => {
   return (
-    <Paper sx={{ p: 2, mb: 2 }} key={commentId}>
+    <Paper sx={{ p: 2, mb: 2 }}>
       <List sx={{ width: "100%", bgcolor: "background.paper" }}>
         <ListItem alignItems="flex-start" disableGutters>
           <ListItemAvatar>
-            <Avatar alt={comment.username} src="/static/images/avatar/1.jpg" />
+            <Avatar alt={comment.username} />
           </ListItemAvatar>
           <ListItemText
             primary={comment.username}
